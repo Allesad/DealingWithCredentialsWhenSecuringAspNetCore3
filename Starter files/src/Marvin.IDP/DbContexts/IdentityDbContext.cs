8cbbd8daca24542dcbc952fd779ee3ddfcfac022
Allesad <allesad.dev@gmail.com>
2020-05-03 23:06:29 +0300

    3.3

---
+++ Starter files/src/Marvin.IDP/DbContexts/IdentityDbContext.cs
@@ -0,0 +1,48 @@
+﻿using Marvin.IDP.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Marvin.IDP.DbContexts
+{
+    public class IdentityDbContext : DbContext
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserClaim> UserClaims { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Subject)
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            // Get updated entities
+            var updatedEntries = ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Modified)
+                .OfType<IConcurrencyAware>();
+
+            foreach (var entry in updatedEntries)
+            {
+                entry.ConcurrencyStamp = Guid.NewGuid().ToString();
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
