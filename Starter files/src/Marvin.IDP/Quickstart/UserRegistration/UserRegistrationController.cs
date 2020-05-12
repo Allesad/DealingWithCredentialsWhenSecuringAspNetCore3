134ae09c5d394e38d8d8b055d7a3cdae9fc08ce7
Allesad <allesad.dev@gmail.com>
2020-05-12 10:16:15 +0300

    4.7

---
+++ Starter files/src/Marvin.IDP/Quickstart/UserRegistration/UserRegistrationController.cs
@@ -0,0 +1,80 @@
+﻿using IdentityModel;
+using IdentityServer4.Services;
+using Marvin.IDP.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Marvin.IDP.UserRegistration
+{
+    public class UserRegistrationController : Controller
+    {
+        private readonly ILocalUserService _localUserService;
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public UserRegistrationController(ILocalUserService localUserService, IIdentityServerInteractionService interaction)
+        {
+            _localUserService = localUserService ?? throw new ArgumentNullException(nameof(localUserService));
+            _interaction = interaction ?? throw new ArgumentNullException(nameof(interaction));
+        }
+
+        [HttpGet]
+        public IActionResult RegisterUser(string returnUrl)
+        {
+            var vm = new RegisterUserViewModel { ReturnUrl = returnUrl };
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var userToCreate = new Entities.User
+            {
+                Username = model.UserName,
+                Password = model.Password,
+                Subject = Guid.NewGuid().ToString(),
+                Active = true
+            };
+
+            userToCreate.Claims.Add(new Entities.UserClaim
+            {
+                Type = "country",
+                Value = model.Country
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim
+            {
+                Type = JwtClaimTypes.Address,
+                Value = model.Address
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim
+            {
+                Type = JwtClaimTypes.GivenName,
+                Value = model.GivenName
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim
+            {
+                Type = JwtClaimTypes.FamilyName,
+                Value = model.FamilyName
+            });
+
+            _localUserService.AddUser(userToCreate);
+            await _localUserService.SaveChangesAsync();
+
+            await HttpContext.SignInAsync(userToCreate.Subject, userToCreate.Username);
+
+            if (_interaction.IsValidReturnUrl(model.ReturnUrl) || Url.IsLocalUrl(model.ReturnUrl))
+            {
+                return Redirect(model.ReturnUrl);
+            }
+
+            return Redirect("~/");
+        }
+    }
+}
