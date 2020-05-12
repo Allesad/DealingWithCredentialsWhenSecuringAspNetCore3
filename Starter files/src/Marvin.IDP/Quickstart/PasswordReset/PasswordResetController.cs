8feabe8463978522a7ce9ee9ce36625db6b808fa
Allesad <allesad.dev@gmail.com>
2020-05-13 01:00:12 +0300

    4.16 Handling password reset

--- Starter files/src/Marvin.IDP/Quickstart/PasswordReset/PasswordResetController.cs
+++ Starter files/src/Marvin.IDP/Quickstart/PasswordReset/PasswordResetController.cs
@@ -33,10 +33,45 @@ namespace Marvin.IDP.PasswordReset
             var securityCode = await _localUserService.InitiatePasswordResetRequest(model.Email);
             await _localUserService.SaveChangesAsync();
 
-            var link = Url.Action("ResetPassword", "PasswordReset", new { securityCode });
+            var link = Url.ActionLink("ResetPassword", "PasswordReset", new { securityCode });
             Debug.WriteLine(link);
 
             return View("PasswordResetRequestSent");
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword(string securityCode)
+        {
+            var vm = new ResetPasswordViewModel
+            {
+                SecurityCode = securityCode
+            };
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await _localUserService.SetPassword(model.SecurityCode, model.Password))
+            {
+                ViewData["Message"] = "Your password was successfully changed. " +
+                    "Navigate to your client application to log in.";
+            }
+            else
+            {
+                ViewData["Message"] = "Your password couldn't be changed, please " +
+                    "contact your administrator";
+            }
+
+            await _localUserService.SaveChangesAsync();
+
+            return View("ResetPasswordResult");
         }
     }
 }
---
+++ Starter files/src/Marvin.IDP/Quickstart/PasswordReset/ResetPasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Marvin.IDP.PasswordReset
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Your new password")]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Config your new password")]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string SecurityCode { get; set; }
+    }
+}
